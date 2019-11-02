a3bb2897bf8dd756c647e83c1a8e771cb6a5e374
Nathan Gray <nathan.gray757@gmail.com>
2019-11-02 11:58:53 -0400

    Added buttons, destroyed speakers

--- app/src/main/java/com/example/ptshe/collegefootballgamepredictor/MainActivity.java
+++ app/src/main/java/com/example/ptshe/collegefootballgamepredictor/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ptshe.collegefootballgamepredictor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonGoToPredictor = findViewById(R.id.buttonGoToPredictor);
+        Button buttonGoToRankings = findViewById(R.id.buttonGoToRankings);
+
+        buttonGoToPredictor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Predictor.class));
+            }
+        });
+        buttonGoToRankings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Rankings.class));
+            }
+        });
+
     }
 }
